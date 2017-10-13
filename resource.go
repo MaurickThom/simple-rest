29dc50089c0a121f384883dae80144cdfc251f65
Alexys Lozada <alexyslc@gmail.com>
2017-10-13 17:32:03 -0500

    Devuelve los diferentes formatos

--- resource.go
+++ resource.go
@@ -8,5 +8,18 @@ import (
 // get handler for all request
 func get(c echo.Context) error {
 	cs := getAll()
-	return c.String(http.StatusOK, cs.String())
+	ct := c.Request().Header.Get("Content-Type")
+
+	switch ct {
+	case "":
+		fallthrough
+	case "text/plain":
+		return c.String(http.StatusOK, cs.String())
+	case "application/json":
+		return c.JSON(http.StatusOK, cs)
+	case "text/xml":
+		return c.XML(http.StatusOK, cs)
+	default:
+		return c.String(http.StatusBadRequest, "Debe especificar un content-type válido")
+	}
 }
